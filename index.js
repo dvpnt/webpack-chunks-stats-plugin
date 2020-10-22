3bf6ff5264aabe23e501968fcdfa1f38e7812a46
Maxim Shtyr <sh.maxim97@gmail.com>
2020-10-22 10:49:22 +0300

    add webpack v5 compatibility

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
+const webpack = require('webpack');
+// webpack v4/v5 compatibility:
+// https://github.com/webpack/webpack/issues/11425#issuecomment-686607633
+const {RawSource} = webpack.sources || require('webpack-sources');
+
 class ChunksStatsPlugin {
 	constructor(options = {}) {
 		this.filename = options.filename || 'stats.json';
 	}
 
 	apply(compiler) {
-		compiler.hooks.emit.tap('ChunksStatsPlugin', (compiler) => {
-			const stats = compiler.getStats().toJson({chunks: true});
-			const mappedStats = {};
+		// webpack v4/v5 compatibility:
+		// https://github.com/webpack/webpack/issues/11425#issuecomment-690387207
+		if (webpack.version.startsWith('4.')) {
+			compiler.hooks.emit.tap(
+				this.constructor.name,
+				(compilation) => this.handleCompilation(compilation)
+			);
+		} else {
+			// Specifically hook into thisCompilation, as per
+			// https://github.com/webpack/webpack/issues/11425#issuecomment-690547848
+			compiler.hooks.thisCompilation.tap(
+				this.constructor.name,
+				(compilation) => {
+					compilation.hooks.processAssets.tap(
+						// See https://github.com/webpack/webpack/blob/9230acbf1a39a8afb2e34f41e2fd7326eef84968/lib/Compilation.js#L3376-L3381
+						{
+							name: this.constructor.name,
+							stage: webpack.Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE
+						},
+						() => this.handleCompilation(compilation)
+					);
+				}
+			);
+		}
+	}
 
-			stats.chunks.forEach(({names, hash}) => {
-				mappedStats[names[0]] = {hash};
-			});
+	handleCompilation(compilation) {
+		const stats = compilation.getStats().toJson({chunks: true});
+		const mappedStats = {};
 
-			const statsStr = JSON.stringify(mappedStats, null, 2);
-
-			compiler.assets[this.filename] = {
-				source: () => statsStr,
-				size: () => statsStr.length
-			};
+		stats.chunks.forEach(({names, hash}) => {
+			mappedStats[names[0]] = {hash};
 		});
+
+		const statsStr = JSON.stringify(mappedStats, null, 2);
+
+		compilation.emitAsset(this.filename, new RawSource(statsStr));
 	}
 }
 
